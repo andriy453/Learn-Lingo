3c62cb6896b3367e26f402320e3999aeb0208c65
andriy <evicandrij@gmail.com>
2023-12-31 20:07:33 +0200

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/react_vite">
+    <BrowserRouter basename="/Learn_Lingo">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
