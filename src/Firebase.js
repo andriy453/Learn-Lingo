78f1606a788d5f595a271e9cdd50783d52aab47f
andriy <evicandrij@gmail.com>
2024-01-22 15:03:17 +0200

    commit

--- src/Firebase.js
+++ src/Firebase.js
@@ -3,9 +3,9 @@ import { GoogleAuthProvider } from 'firebase/auth';
 import { initializeAuth } from 'firebase/auth';
 import { getDatabase } from 'firebase/database';
 
-const API_KEY = import.meta.env.VITE_API_KEY;
+// const API_KEY = import.meta.env.VITE_API_KEY;
 const firebaseConfig = {
-  apiKey: API_KEY,
+  apiKey:"AIzaSyDkWC69rklVP4x89l4iBxXBRgp9ClD9p40",
   authDomain: 'test-582b2.firebaseapp.com',
   projectId: 'test-582b2',
   storageBucket: 'test-582b2.appspot.com',
